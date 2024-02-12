44bfb0b8b849fcefbb86a0ccc46bff92d912fe90
Vadimshidlov <vadim.shidlov@gmail.com>
2024-02-12 18:22:44 +0300

    feat: implement tests for crud-api

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -7,4 +7,57 @@ describe('Tests for crud-api ;)', () => {
 
         expect(res.statusCode).toEqual(200);
     });
+
+    it('should return correct data with 4 users', async () => {
+        const res = await request(server).get('/api/users');
+        const data = res.body;
+
+        expect(data.length).toBe(4);
+    });
+
+    it('should create correct data for user with post method', async () => {
+        const userData = {
+            userName: 'Oleg',
+            age: 26,
+            hobbies: ['Gym'],
+        };
+
+        const res = await request(server).post('/api/users').send(userData);
+        const data = res.body;
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { id, ...rest } = data;
+
+        expect(rest).toStrictEqual(userData);
+    });
+
+    it('should return correct status code for create user', async () => {
+        const userData = {
+            userName: 'Oleg',
+            age: 26,
+            hobbies: ['Gym'],
+        };
+
+        const res = await request(server).post('/api/users').send(userData);
+
+        expect(res.statusCode).toBe(201);
+    });
+
+    it('should allow to get date of user we created', async () => {
+        const userData = {
+            userName: 'Oleg',
+            age: 26,
+            hobbies: ['Gym'],
+        };
+
+        const postRes = await request(server).post('/api/users').send(userData);
+
+        expect(postRes.statusCode).toBe(201);
+
+        const { id } = postRes.body;
+
+        const getRes = await request(server).get(`/api/users/${id}`);
+
+        expect(getRes.statusCode).toBe(200);
+    });
 });
