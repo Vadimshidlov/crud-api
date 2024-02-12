df9d1e380cd8237078708ed81bd5b1b6cfcad9a5
Vadimshidlov <vadim.shidlov@gmail.com>
2024-02-12 22:23:32 +0300

    feat: implement test for put and delete crud-api logic

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,6 +2,10 @@ import { server } from '../index';
 import request from 'supertest';
 
 describe('Tests for crud-api ;)', () => {
+    afterAll((done) => {
+        server.close(done);
+    });
+
     it('should return correct status code', async () => {
         const res = await request(server).get('/api/users');
 
@@ -59,5 +63,69 @@ describe('Tests for crud-api ;)', () => {
         const getRes = await request(server).get(`/api/users/${id}`);
 
         expect(getRes.statusCode).toBe(200);
+    });
+
+    // should update user with method put
+    it('should update user and return correct data with the same id', async () => {
+        const userData = {
+            userName: 'Oleg',
+            age: 26,
+            hobbies: ['Gym'],
+        };
+
+        const newUserData = {
+            userName: 'Vadim',
+            age: 26,
+            hobbies: ['Fishing'],
+        };
+
+        const postRes = await request(server).post('/api/users').send(userData);
+
+        expect(postRes.statusCode).toBe(201);
+
+        const { id } = postRes.body;
+
+        const getRes = await request(server).get(`/api/users/${id}`);
+
+        expect(getRes.statusCode).toBe(200);
+
+        const putRes = await request(server).put(`/api/users/${id}`).send(newUserData);
+
+        expect(putRes.statusCode).toBe(200);
+
+        const strictUserData = {
+            id,
+            ...newUserData,
+        };
+
+        expect(putRes.body).toStrictEqual(strictUserData);
+    });
+
+    it('should correct delete user and return correct status code', async () => {
+        const getRes = await request(server).get(`/api/users`);
+
+        expect(getRes.statusCode).toBe(200);
+
+        const { id: theFirstUserId } = getRes.body[0];
+
+        const deleteRes = await request(server).delete(`/api/users/${theFirstUserId}`);
+
+        expect(deleteRes.statusCode).toBe(204);
+    });
+
+    it('should return correct status code for getting already delete user', async () => {
+        const getRes = await request(server).get(`/api/users`);
+
+        expect(getRes.statusCode).toBe(200);
+
+        const { id: theFirstUserId } = getRes.body[0];
+
+        const deleteRes = await request(server).delete(`/api/users/${theFirstUserId}`);
+
+        expect(deleteRes.statusCode).toBe(204);
+
+        const getResSecond = await request(server).get(`/api/users${theFirstUserId}`);
+
+        expect(getResSecond.statusCode).toBe(404);
     });
 });
